28af503fbb016657e4d75f3b93228f2424556373
Bartosz Kaszewczuk <bartosz.kaszewczuk@icloud.com>
2024-03-13 00:19:32 +0100

    Refactor client.tsx

--- src/app/outgoing/traefik/client.tsx
+++ src/app/outgoing/traefik/client.tsx
@@ -1,22 +1,46 @@
-import {TraefikRouter} from "@/app/outgoing/traefik/models";
+import {TraefikEntryPoint, TraefikRouter} from "@/app/outgoing/traefik/models";
 import {ENDPOINT_ROUTERS} from "@/app/outgoing/traefik/config";
+import voca from "voca";
 
-export async function getRulesFromRouters(): Promise<Array<TraefikRouter>> {
-    const response = await fetch(ENDPOINT_ROUTERS)
+function ruleToRoute(rule: string) {
+    const items = voca.split(rule).map(subRule => subRule.substring(subRule.indexOf("`")+1, subRule.lastIndexOf("`")))
+    return `https://${items[0]}`
+}
 
+async function httpGetBody(url: string) {
+    const response = await fetch(url)
     if (!response.ok) {
         // This will activate the closest `error.js` Error Boundary
-        throw new Error(`Error calling endpoint ${ENDPOINT_ROUTERS}: ${response.status}`)
+        throw new Error(`Error calling endpoint ${url}: ${response.status}`)
     }
+    return response.json();
+}
 
-    const body = await response.json()
-    return body.map((router: any) => {
+export async function getRules(): Promise<Array<TraefikRouter>> {
+    const routes = await httpGetBody(ENDPOINT_ROUTERS);
+    return routes.map((router: any) => {
             const out: TraefikRouter = {
-                name: router.name,
-                rule: router.rule
+                provider: router.provider,
+                name: voca.capitalize(router.name),
+                rule: ruleToRoute(router.rule),
+                entryPoint: router.entryPoints[0]
             }
             return out;
         }
     )
 }
+
+export async function getEntryPoints(): Promise<Array<TraefikEntryPoint>> {
+    const entryPoints = await httpGetBody(ENDPOINT_ROUTERS);
+    return entryPoints.map((entryPoint: any) => {
+            const out: TraefikEntryPoint = {
+                name: entryPoint.name,
+                port: entryPoint.address
+            }
+            return out;
+        }
+    )
+}
+
+
 
