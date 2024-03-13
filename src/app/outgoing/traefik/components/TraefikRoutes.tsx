b6536aae9db1a41169abe2224cd0b2821ba6ea19
Bartosz Kaszewczuk <bartosz.kaszewczuk@icloud.com>
2024-03-13 13:08:50 +0100

    Autogenerate rows from object fields

--- src/app/outgoing/traefik/components/TraefikRoutes.tsx
+++ src/app/outgoing/traefik/components/TraefikRoutes.tsx
@@ -1,19 +1,20 @@
 'use client';
 import React, {use} from "react";
 import {  Table,  TableHeader,  TableBody,  TableColumn,  TableRow,  TableCell} from "@nextui-org/table";
-import {TraefikRouter} from "@/app/outgoing/traefik/models";
+import {TraefikRouter, TrafiService} from "@/app/outgoing/traefik/models";
 import {capitalize} from "@nextui-org/shared-utils";
+import {Link} from "@nextui-org/link";
 
-export function TraefikRoutes({traefikRoutes}: { traefikRoutes: Array<TraefikRouter> }) {
-    const rules = traefikRoutes;
-    const rows = rules.map((route, index) => {
+export function TraefikRoutes({trafiServices}: { trafiServices: TrafiService[] }) {
+    const services = trafiServices;
+    const rows = services.map((route, index) => {
             return {
                 key: index,
                 obj: route,
             }
         }
     )
-    const columns = Object.keys(rules[0]).map((route, index) => {
+    const columns = Object.keys(services[0]).map((route, index) => {
         return {
             key: index,
             obj: route,
@@ -21,18 +22,20 @@ export function TraefikRoutes({traefikRoutes}: { traefikRoutes: Array<TraefikRou
     });
 
     return (
+        // <Link
+        //     isExternal
+        //     href="https://github.com/nextui-org/nextui"
+        //     showAnchorIcon
+        // ></Link>
         <Table aria-label="Example static collection table">
             <TableHeader columns={columns}>
                 {(column) => <TableColumn key={column.key}>{capitalize(column.obj)}</TableColumn>}
             </TableHeader>
             <TableBody items={rows}>
-                {
-                    (row) =>
-                        <TableRow key={row.key}>
-                            <TableCell>{row.obj.name}</TableCell>
-                            <TableCell>{row.obj.rule}</TableCell>
-                        </TableRow>
-
+                {(row) =>
+                    <TableRow key={row.key}>
+                        {Object.keys(row.obj).map((fieldName, index) => <TableCell key={index}>{row.obj[fieldName]}</TableCell>)}
+                    </TableRow>
                 }
             </TableBody>
         </Table>
