eac64cf8f8ea2850044672af10661f7d0d32ac0a
Bartosz Kaszewczuk <bartosz.kaszewczuk@icloud.com>
2024-03-13 13:08:06 +0100

    Render TraefikRoutes on page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,16 @@
-// 'use client';
 import React from "react";
 import { use } from "react";
-import {Table, TableBody, TableCell, TableColumn, TableHeader, TableRow} from "@nextui-org/table";
 import {Button} from "@nextui-org/button";
 import {TraefikRoutes} from "@/app/outgoing/traefik/components/TraefikRoutes";
-import {getRulesFromRouters} from "@/app/outgoing/traefik/client";
+import {getTrafiServices} from "@/app/outgoing/traefik/client";
 
 export default function Home() {
-    const items = use(getRulesFromRouters());
-    // const items = TraefikRoutes;
-    const dupa = <ol>
-        <li>dupa</li>
-    </ol>
+    const services = use(getTrafiServices());
 
-    return (<main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <TraefikRoutes traefikRoutes={items}></TraefikRoutes>
-                    {/*{items}*/}
-                    {/*<TraefikRoutes></TraefikRoutes>*/}
-                    {/*<Table>*/}
-                        {/*<TableHeader>*/}
-                        {/*    <TableColumn key="name">Name</TableColumn>*/}
-                        {/*    /!*<TableColumn key="route">Route</TableColumn>*!/*/}
-                        {/*</TableHeader>*/}
-                        {/*<TableBody>*/}
-                        {/*    <TableRow key="name2">*/}
-                        {/*        <TableCell>name</TableCell>*/}
-                        {/*        /!*<TableCell>rule</TableCell>*!/*/}
-                        {/*    </TableRow>*/}
-                        {/*</TableBody>*/}
-                        {/*<TableBody emptyContent={"No rows to display."}>{[]}</TableBody>*/}
-
-                    {/*</Table>*/}
-                    <Button>Click Me</Button>
-    </main>)
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <TraefikRoutes trafiServices={services}></TraefikRoutes>
+            <Button>Click Me</Button>
+        </main>
+    )
 }
