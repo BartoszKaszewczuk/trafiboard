583f779eaadb352db77450ae7d9ff4998da824db
Bartosz Kaszewczuk <bartosz.kaszewczuk@icloud.com>
2024-03-13 20:59:03 +0100

    Cleanup

--- src/app/outgoing/traefik/components/TraefikRoutes.tsx
+++ src/app/outgoing/traefik/components/TraefikRoutes.tsx
@@ -4,9 +4,11 @@ import {  Table,  TableHeader,  TableBody,  TableColumn,  TableRow,  TableCell} 
 import {TraefikRouter, TrafiService} from "@/app/outgoing/traefik/models";
 import {capitalize} from "@nextui-org/shared-utils";
 import {Link} from "@nextui-org/link";
+// import _ from "lodash";
 
 export function TraefikRoutes({trafiServices}: { trafiServices: TrafiService[] }) {
     const services = trafiServices;
+    // const servicesGroupedByProvider = _.groupBy(services, 'provider')
     const rows = services.map((route, index) => {
             return {
                 key: index,
@@ -22,11 +24,6 @@ export function TraefikRoutes({trafiServices}: { trafiServices: TrafiService[] }
     });
 
     return (
-        // <Link
-        //     isExternal
-        //     href="https://github.com/nextui-org/nextui"
-        //     showAnchorIcon
-        // ></Link>
         <Table aria-label="Example static collection table">
             <TableHeader columns={columns}>
                 {(column) => <TableColumn key={column.key}>{capitalize(column.obj)}</TableColumn>}
