253f35c61602e3f529c9f935dd3cda6bddf1956e
Bartosz Kaszewczuk <bartosz.kaszewczuk@icloud.com>
2024-03-20 21:18:49 +0100

    Render only TrafiServiceList component on page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
-'use client';
+// 'use client';
 import React from "react";
 import { use } from "react";
-import {Button} from "@nextui-org/button";
-import {TraefikRoutes} from "@/app/outgoing/traefik/components/TraefikRoutes";
+// import {Button} from "@nextui-org/button";
+// import {TraefikRoutes} from "@/app/outgoing/traefik/components/TraefikRoutes";
 import {getTrafiServices} from "@/app/outgoing/traefik/client";
 import {TrafiServiceList} from "@/app/outgoing/traefik/components/TrafiServiceList";
-import {Image} from "@nextui-org/react";
+// import {TraefikRoutes} from "@/app/outgoing/traefik/components/TraefikRoutes";
+// import {Divider, Image} from "@nextui-org/react";
 
 export default function Home() {
     const services = use(getTrafiServices());
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <Image height={100} width={100} src="/vercel.svg"></Image>
-            <TrafiServiceList trafiServices={services}></TrafiServiceList>
-            <TraefikRoutes trafiServices={services}></TraefikRoutes>
-            <Button>Click Me</Button>
+                {/*<Image height={100} width={100} src="/vercel.svg"></Image>*/}
+                <TrafiServiceList trafiServices={services}></TrafiServiceList>
+                {/*<Divider orientation="horizontal"/>*/}
+                {/*<TraefikRoutes trafiServices={services}></TraefikRoutes>*/}
+                {/*<Button>Click Me</Button>*/}
         </main>
-    )
+)
 }
