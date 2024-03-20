ff6db299d2cb8e222c807b470145424e0a248731
Bartosz Kaszewczuk <bartosz.kaszewczuk@icloud.com>
2024-03-20 20:32:49 +0100

    Remove Chakra

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,14 +2,11 @@
 
 import React from "react";
 import {NextUIProvider} from '@nextui-org/react'
-import {ChakraProvider} from "@chakra-ui/react";
 
 export function Providers({children}: { children: React.ReactNode }) {
     return (
         <NextUIProvider>
-            <ChakraProvider>
             {children}
-            </ChakraProvider>
         </NextUIProvider>
     )
 }
