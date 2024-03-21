00a21f8dbe3f8deb4a5555a01b663cbccc10d237
Bartosz Kaszewczuk <bartosz.kaszewczuk@icloud.com>
2024-03-21 20:10:37 +0100

    Extract TrafiServiceCard into its own component

---
+++ src/app/outgoing/traefik/components/TrafiServiceCard.tsx
@@ -0,0 +1,34 @@
+'use client';
+import {Card, CardFooter, Image, CardBody} from "@nextui-org/react";
+
+
+export function TrafiServiceCard({serviceName, serviceRoute}) {
+    return (
+        <div key={serviceName} className="text-wrap">
+            <Card
+                isFooterBlurred
+                isPressable
+                onPress={(e) => window.open(serviceRoute)}
+                radius="lg"
+                className="border-none object-cover"
+                fullWidth={true}
+            >
+                <CardBody className="overflow-visible p-0">
+                    <Image
+                        height={150}
+                        width={150}
+                        src="https://picsum.photos/150"
+                    ></Image>
+                </CardBody>
+                <CardFooter
+                    className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+                    {/*<Link*/}
+                    {/*    className="text-white inline-block"*/}
+                    {/*    href={serviceRoute}*/}
+                    {/*>{serviceName}</Link>*/}
+                    <p className="inline-block text-white/80">{serviceName}</p>
+                </CardFooter>
+            </Card>
+        </div>
+    )
+}
--- src/app/outgoing/traefik/components/TrafiServiceList.tsx
+++ src/app/outgoing/traefik/components/TrafiServiceList.tsx
@@ -1,54 +1,16 @@
 import {TrafiService} from "@/app/outgoing/traefik/models";
-import {Link} from "@nextui-org/link";
-import {Card, CardFooter, Image, Button, CardBody} from "@nextui-org/react";
+import {TrafiServiceCard} from "@/app/outgoing/traefik/components/TrafiServiceCard";
 
 
 export function TrafiServiceList({trafiServices}: { trafiServices: TrafiService[] }) {
-    const trafi = trafiServices
+    const trafiServicesFiltered = trafiServices
         .filter(service => service.provider != "internal")
         .sort((a, b) => a.name.localeCompare(b.name))
-        .map(service => {
-        return (
-            <div key={service.name} className="text-wrap">
-                <Card
-                    isFooterBlurred
-                    isPressable
-                    radius="lg"
-                    className="border-none"
-                    fullWidth={true}
-                >
-                    <CardBody className="overflow-visible p-0">
-                        <Image
-                            height={150}
-                            width={150}
-                            src="https://picsum.photos/150"
-                            ></Image>
-                    </CardBody>
-                    {/*<Image*/}
-                    {/*    alt="Woman listing to music"*/}
-                    {/*    className="object-cover"*/}
-                    {/*    height={200}*/}
-                    {/*    src="/vercel.svg"*/}
-                    {/*    width={200}*/}
-                    {/*/>*/}
-                    <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-                        <Link
-                            className="text-white inline-block"
-                            href={service.getRoutes()[0]}
-                        >{service.getCleanName()}</Link>
-                        {/*<p className="text-tiny text-white/80">Available soon.</p>*/}
-                        {/*<Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm" href={service.getRoutes()[0]}>*/}
-                        {/*    {service.getCleanName()}*/}
-                        {/*</Button>*/}
-                    </CardFooter>
-                </Card>
-            </div>
-        )
-    })
+        .map((service) => <TrafiServiceCard key={service.name} serviceName={service.getCleanName()} serviceRoute={service.getRoutes()[0]}></TrafiServiceCard> )
 
     const wrapped = (
         <div className="grid grid-cols-4 gap-8">
-            {trafi}
+            {trafiServicesFiltered}
         </div>
     )
     return wrapped;
