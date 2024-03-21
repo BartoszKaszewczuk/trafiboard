ce0a917e6ba88143721459a00a86109bd2abb4e2
Bartosz Kaszewczuk <bartosz.kaszewczuk@icloud.com>
2024-03-21 01:11:36 +0100

    Restyle elements of TrafiServiceList

--- src/app/outgoing/traefik/components/TrafiServiceList.tsx
+++ src/app/outgoing/traefik/components/TrafiServiceList.tsx
@@ -1,5 +1,6 @@
 import {TrafiService} from "@/app/outgoing/traefik/models";
 import {Link} from "@nextui-org/link";
+import {Card, CardFooter, Image, Button, CardBody} from "@nextui-org/react";
 
 
 export function TrafiServiceList({trafiServices}: { trafiServices: TrafiService[] }) {
@@ -9,10 +10,33 @@ export function TrafiServiceList({trafiServices}: { trafiServices: TrafiService[
         .map(service => {
         return (
             <div key={service.name} className="text-wrap">
-                <Link
-                    href={service.getRoutes()[0]}
-                    showAnchorIcon
-                >{service.getCleanName()}</Link>
+                <Card
+                    isFooterBlurred
+                    isPressable
+                    radius="lg"
+                    className="border-none"
+                >
+                    <CardBody className="overflow-visible p-0">
+                        <Link
+                            className="h-30 text-white bg-black/20 inline-block"
+                            href={service.getRoutes()[0]}
+                            showAnchorIcon
+                        >{service.getCleanName()}</Link>
+                    </CardBody>
+                    {/*<Image*/}
+                    {/*    alt="Woman listing to music"*/}
+                    {/*    className="object-cover"*/}
+                    {/*    height={200}*/}
+                    {/*    src="/vercel.svg"*/}
+                    {/*    width={200}*/}
+                    {/*/>*/}
+                    <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+                        {/*<p className="text-tiny text-white/80">Available soon.</p>*/}
+                        {/*<Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm" href={service.getRoutes()[0]}>*/}
+                        {/*    {service.getCleanName()}*/}
+                        {/*</Button>*/}
+                    </CardFooter>
+                </Card>
             </div>
         )
     })
