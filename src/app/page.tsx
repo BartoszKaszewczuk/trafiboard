d780ac750017f3dcf645201825a369e894ff7cf7
Bartosz Kaszewczuk <bartosz.kaszewczuk@icloud.com>
2024-03-29 17:26:49 +0100

    Make header clickable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { use } from "react";
 import {getTrafiServices} from "@/app/outgoing/traefik/client";
 import {TrafiServiceList} from "@/app/outgoing/traefik/components/TrafiServiceList";
-import {Divider} from "@nextui-org/react";
+import {Divider, Link} from "@nextui-org/react";
 
 export default function Home() {
     const services = use(getTrafiServices());
@@ -11,9 +11,12 @@ export default function Home() {
     return (
         <main className="container min-h-screen ml:px-5 m-auto flex-col items-center justify-between p-24">
             <div className="h-20">
-                <span className="mb-1.5 text-xl inline-block rounded-full bg-gray-500/50 py-2 px-6 text-white/90">
+                    <Link
+                        className="mb-1.5 text-xl inline-block rounded-full bg-gray-500/50 py-2 px-6 text-white/90"
+                        href="#"
+                    >
                     Kaszewczuk's Apps
-                </span>
+                    </Link>
             </div>
             {/*<Image height={100} width={100} src="/vercel.svg"></Image>*/}
             {/*<Divider orientation="horizontal"/>*/}
