634ca857df982f04d66e7633ae7871ac9a37ab72
Bartosz Kaszewczuk <bartosz.kaszewczuk@icloud.com>
2024-04-07 12:49:13 +0200

    Wrap card body with a Link to make url visible in browsers

--- src/app/outgoing/traefik/components/TrafiServiceCard.tsx
+++ src/app/outgoing/traefik/components/TrafiServiceCard.tsx
@@ -1,5 +1,5 @@
 'use client';
-import {Card, CardFooter, Image, CardBody} from "@nextui-org/react";
+import {Card, CardFooter, Image, CardBody, Link} from "@nextui-org/react";
 
 
 export function TrafiServiceCard({serviceName, serviceRoute}) {
@@ -13,27 +13,29 @@ export function TrafiServiceCard({serviceName, serviceRoute}) {
                 className="border-none object-cover"
                 fullWidth={true}
             >
-                <CardBody className="overflow-visible p-0">
-                    <Image
-                        isZoomed
-                        height="100%"
-                        width="100%"
-                        src="https://picsum.photos/350/150"
-                        // src={serviceRoute + '/favicon.ico'}
-                    ></Image>
-                </CardBody>
-                <CardFooter
-                    className="before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10"
-                >
-                    <div className="h-6 mr-3">
+                <Link href={serviceRoute}>
+                    <CardBody className="overflow-visible p-0">
                         <Image
+                            isZoomed
                             height="100%"
-                            className="h-6"
-                            src={serviceRoute + '/favicon.ico'}
+                            width="100%"
+                            src="https://picsum.photos/350/150"
+                            // src={serviceRoute + '/favicon.ico'}
                         ></Image>
-                    </div>
-                    <p className="inline-block text-white/80">{serviceName}</p>
-                </CardFooter>
+                    </CardBody>
+                    <CardFooter
+                        className="before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10"
+                    >
+                        <div className="h-6 mr-3">
+                            <Image
+                                height="100%"
+                                className="h-6"
+                                src={serviceRoute + '/favicon.ico'}
+                            ></Image>
+                        </div>
+                        <p className="inline-block text-white/80">{serviceName}</p>
+                    </CardFooter>
+                </Link>
             </Card>
         </div>
     )
