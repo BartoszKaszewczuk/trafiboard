45c8f4266763c62494617b094ae96813765516cd
Bartosz Kaszewczuk <bartosz.kaszewczuk@icloud.com>
2025-04-24 17:40:04 +0200

    Add URL footer on the bottom of the card

--- src/app/outgoing/traefik/components/TrafiServiceCard.tsx
+++ src/app/outgoing/traefik/components/TrafiServiceCard.tsx
@@ -14,20 +14,21 @@ export function TrafiServiceCard({serviceName, serviceRoute, thumbnailUrl}: {ser
                 fullWidth={true}
             >
                 <Link href={serviceRoute} target={"_blank"}>
-                    <CardBody className="overflow-visible p-0">
+                    {/*<CardBody className="overflow-visible p-0">*/}
                         <Image
                             isZoomed
                             height="100%"
                             width="100%"
                             // src={nullAsUndefined(thumbnailUrl) ? thumbnailUrl : "https://picsum.photos/350/150"}
                             src={nullAsUndefined(thumbnailUrl) ? thumbnailUrl!! : "/default-service-thumbnail.jpeg"}
+                            // src={"https://picsum.photos/350/200"}
                             alt={`thumbnail-${serviceName}`}
                         ></Image>
-                    </CardBody>
-                    <CardFooter
-                        className="before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10"
+                    {/*</CardBody>*/}
+                    <CardBody
+                        className="backdrop-blur-sm bg-gray-500/30 border-gray-400 before:bg-white/10 border-white/20 border-1 flex-row overflow-hidden py-1 z-20 absolute before:rounded-xl rounded-large bottom-9 w-[calc(100%_-_8px)] shadow-small ml-1"
                     >
-                        <div className="h-6 w-6 mr-3">
+                        <div className="h-6 w-6 mr-3 inline-block">
                             <Image
                                 height="100%"
                                 width="100%"
@@ -38,6 +39,16 @@ export function TrafiServiceCard({serviceName, serviceRoute, thumbnailUrl}: {ser
                         <p className="inline-block text-white/80">
                             {serviceName}
                         </p>
+                    </CardBody>
+                    <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between p-3 pt-1.5 pb-1.5">
+                        {/*<div>*/}
+                            <p className="text-tiny text-white/80">{serviceRoute.split("://")[1]}</p>
+                            {/*<p className="text-black text-tiny">Available soon.</p>*/}
+                            {/*<p className="text-black text-tiny">Get notified.</p>*/}
+                        {/*</div>*/}
+                        {/*<Button className="text-tiny" color="primary" radius="full" size="sm">*/}
+                        {/*    Notify Me*/}
+                        {/*</Button>*/}
                     </CardFooter>
                 </Link>
             </Card>
