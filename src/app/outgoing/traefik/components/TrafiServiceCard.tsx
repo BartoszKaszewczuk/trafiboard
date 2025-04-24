526e16f6b88a1933129693653a2c9ffc57633532
Bartosz Kaszewczuk <bartosz.kaszewczuk@icloud.com>
2025-04-24 20:11:17 +0200

    Add fine text with route's url on the bottom of each card

--- src/app/outgoing/traefik/components/TrafiServiceCard.tsx
+++ src/app/outgoing/traefik/components/TrafiServiceCard.tsx
@@ -1,13 +1,17 @@
 'use client';
 import {Card, CardFooter, Image, CardBody, Link} from "@heroui/react";
-import { nullAsUndefined } from "null-as-undefined";
+import {nullAsUndefined} from "null-as-undefined";
 
-export function TrafiServiceCard({serviceName, serviceRoute, thumbnailUrl}: {serviceName: string; serviceRoute: string; thumbnailUrl: string | null}) {
+export function TrafiServiceCard({serviceName, serviceRoute, thumbnailUrl}: {
+    serviceName: string;
+    serviceRoute: string;
+    thumbnailUrl: string | null;
+}) {
+    const faviconUrl = serviceRoute + '/favicon.ico'
     return (
         <div key={serviceName} className="text-wrap">
             <Card
                 isFooterBlurred
-                isPressable
                 onPress={(e) => window.open(serviceRoute)}
                 radius="lg"
                 className="border-none object-cover"
@@ -15,40 +19,37 @@ export function TrafiServiceCard({serviceName, serviceRoute, thumbnailUrl}: {ser
             >
                 <Link href={serviceRoute} target={"_blank"}>
                     {/*<CardBody className="overflow-visible p-0">*/}
-                        <Image
-                            isZoomed
-                            height="100%"
-                            width="100%"
-                            // src={nullAsUndefined(thumbnailUrl) ? thumbnailUrl : "https://picsum.photos/350/150"}
-                            src={nullAsUndefined(thumbnailUrl) ? thumbnailUrl!! : "/default-service-thumbnail.jpeg"}
-                            // src={"https://picsum.photos/350/200"}
-                            alt={`thumbnail-${serviceName}`}
-                        ></Image>
+                    <Image
+                        isZoomed
+                        height="130px"
+                        width="100%"
+                        // src={nullAsUndefined(thumbnailUrl) ? thumbnailUrl : "https://picsum.photos/350/150"}
+                        src={nullAsUndefined(thumbnailUrl) ? thumbnailUrl!! : "/default-service-thumbnail.jpeg"}
+                        // src={"https://picsum.photos/350/200"}
+                        alt={`thumbnail-${serviceName}`}
+                    />
                     {/*</CardBody>*/}
                     <CardBody
                         className="backdrop-blur-sm bg-gray-500/30 border-gray-400 before:bg-white/10 border-white/20 border-1 flex-row overflow-hidden py-1 z-20 absolute before:rounded-xl rounded-large bottom-9 w-[calc(100%_-_8px)] shadow-small ml-1"
                     >
                         <div className="h-6 w-6 mr-3 inline-block">
                             <Image
+                                isBlurred
                                 height="100%"
                                 width="100%"
                                 className="h-6"
-                                src={serviceRoute + '/favicon.ico'}
+                                src={faviconUrl}
                             ></Image>
                         </div>
                         <p className="inline-block text-white/80">
                             {serviceName}
                         </p>
                     </CardBody>
-                    <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between p-3 pt-1.5 pb-1.5">
-                        {/*<div>*/}
-                            <p className="text-tiny text-white/80">{serviceRoute.split("://")[1]}</p>
-                            {/*<p className="text-black text-tiny">Available soon.</p>*/}
-                            {/*<p className="text-black text-tiny">Get notified.</p>*/}
-                        {/*</div>*/}
-                        {/*<Button className="text-tiny" color="primary" radius="full" size="sm">*/}
-                        {/*    Notify Me*/}
-                        {/*</Button>*/}
+                    <CardFooter
+                        className="absolute bg-gray-500/30 border-gray-400 bottom-0 border-t-1 border-white/20 z-10 justify-between p-3 pt-1.5 pb-1.5">
+                        <p className="text-tiny text-white/70 shadow overflow-hidden">
+                            {serviceRoute.split("://")[1]}
+                        </p>
                     </CardFooter>
                 </Link>
             </Card>
