0e0b8dc4afcbc15e8d4228c7a67ca4da21641b29
Bartosz Kaszewczuk <bartosz.kaszewczuk@icloud.com>
2025-04-26 02:09:30 +0200

    Render NGINX routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,23 @@ import {Link} from "@heroui/react";
 import {getScreenshot} from "@/app/outgoing/screenshots/client";
 import {TrafiService, TrafiServicePresentable} from "@/app/outgoing/traefik/models";
 import {TRAEFIK_HOSTS} from "@/app/outgoing/traefik/config";
-import {TrafiServicePresentableType } from "TrafiTypes";
+import {TrafiServicePresentableType} from "TrafiTypes";
 import {TrafiServiceListGroupedFiltered} from "@/app/outgoing/traefik/components/TrafiServiceListGroupedFiltered";
-
+import {NginxClient} from "@/app/outgoing/nginx/NginxClient";
 
 function fetchTrafiServicesFromHosts(): Map<string, TrafiService[]> {
     return use(getTrafiServicesFromHosts(TRAEFIK_HOSTS));
 }
 
-function fetchPresentableTrafiServicesType() {
-    const hosts: Map<string, TrafiService[]> = fetchTrafiServicesFromHosts()
+function fetchTrafiServicesFromNginxHosts(): Map<string, TrafiService[]> {
+    return use(NginxClient.getTrafiServicesFromHosts(TRAEFIK_HOSTS));
+}
+
+function fetchPresentableTrafiServicesType(): Map<string, TrafiServicePresentableType[]> {
+    const hostsTraefik: Map<string, TrafiService[]> = fetchTrafiServicesFromHosts()
+    const hostsNginx: Map<string, TrafiService[]> = fetchTrafiServicesFromNginxHosts()
+    const hosts = new Map([...hostsTraefik, ...hostsNginx]);
+
     const mapOfPresentableServices = new Map<string, TrafiServicePresentableType[]>()
     for (const [host, services] of hosts.entries()) {
         const presentables: TrafiServicePresentableType[] = services.map((service: TrafiService) => {
@@ -26,18 +33,19 @@ function fetchPresentableTrafiServicesType() {
     return mapOfPresentableServices
 }
 
+
 export default function Home() {
     const servicesPresentable = fetchPresentableTrafiServicesType();
 
     return (
         <main className="container min-h-screen ml:px-5 m-auto flex-col items-center justify-between p-24">
             <div className="h-20">
-                    <Link
-                        className="mb-1.5 text-3xl inline-block rounded-full bg-gray-500/30 py-2 px-6 text-white/90 backdrop-blur-sm border-1 border-white/10"
-                        href="#"
-                    >
-                        {process.env.TRAFI_TITLE}
-                    </Link>
+                <Link
+                    className="mb-1.5 text-3xl inline-block rounded-full bg-gray-500/30 py-2 px-6 text-white/90 backdrop-blur-sm border-1 border-white/10"
+                    href="#"
+                >
+                    {process.env.TRAFI_TITLE}
+                </Link>
             </div>
             <TrafiServiceListGroupedFiltered trafiServicesMap={servicesPresentable}></TrafiServiceListGroupedFiltered>
             {/*<TrafiTabs>*/}
