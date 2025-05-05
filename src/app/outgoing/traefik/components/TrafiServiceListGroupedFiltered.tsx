0ac117c347cf30f1be66e6a6462da07b238d3c60
Bartosz Kaszewczuk <bartosz.kaszewczuk@icloud.com>
2025-05-05 15:49:10 +0200

    Use clean host names in tabs

--- src/app/outgoing/traefik/components/TrafiServiceListGroupedFiltered.tsx
+++ src/app/outgoing/traefik/components/TrafiServiceListGroupedFiltered.tsx
@@ -7,6 +7,19 @@ import {Switch} from "@heroui/switch";
 import {TrafiServiceListGrouped} from "@/app/outgoing/traefik/components/TrafiServiceListGrouped";
 import {Tab, Tabs} from "@heroui/react";
 import {FaMagnifyingGlass} from "react-icons/fa6";
+import {isNullOrUndefined} from "@/app/utils";
+
+function getTabTitle(maybeTitle: string): string {
+    if (isNullOrUndefined(maybeTitle)) {
+        return maybeTitle
+    }
+
+    if (maybeTitle.includes("://")) {
+        return maybeTitle.split("://")[1]
+    } else {
+        return maybeTitle
+    }
+}
 
 export const TrafiServiceListGroupedFiltered: FC<TrafiServicePresentableMap> = ({trafiServicesMap}) => {
     const [query, setQuery] = useState('')
@@ -39,46 +52,48 @@ export const TrafiServiceListGroupedFiltered: FC<TrafiServicePresentableMap> = (
                           panel: "bg-transparent px-0",
                       }}
                 >
-                    {Array.from(mergedServices.entries()).map(([host, services], index) =>
-                        <Tab key={host} title={host}>
-                            {host.toLowerCase() === "all" ?
-                                <div>
-                                    <div
-                                        className="flex flex-row p-1 mb-5 rounded-lg backdrop-blur-sm bg-gray-500/30 border-1 border-white/30">
-                                        <div className="flex-auto w-80">
-                                            <Input
-                                                type="text"
-                                                placeholder="Filter services..."
-                                                variant={"faded"}
-                                                radius={"sm"}
-                                                classNames={{
-                                                    input: "bg-red-700",
-                                                    innerWrapper: "bg-transparent",
-                                                }}
-                                                startContent={
-                                                    <FaMagnifyingGlass/>
-                                                }
-                                                autoFocus
-                                                value={query}
-                                                onChange={e => setQuery(e.target.value)}
-                                            />
+                    {Array.from(mergedServices.entries()).map(([hostUrl, services], index) => {
+                            const host = getTabTitle(hostUrl)
+                            return (<Tab key={host} title={host}>
+                                {host.toLowerCase() === "all" ?
+                                    <div>
+                                        <div
+                                            className="flex flex-row p-1 mb-5 rounded-lg backdrop-blur-sm bg-gray-500/30 border-1 border-white/30">
+                                            <div className="flex-auto w-80">
+                                                <Input
+                                                    type="text"
+                                                    placeholder="Filter services..."
+                                                    variant={"faded"}
+                                                    radius={"sm"}
+                                                    classNames={{
+                                                        input: "bg-red-700",
+                                                        innerWrapper: "bg-transparent",
+                                                    }}
+                                                    startContent={
+                                                        <FaMagnifyingGlass/>
+                                                    }
+                                                    autoFocus
+                                                    value={query}
+                                                    onChange={e => setQuery(e.target.value)}
+                                                />
+                                            </div>
+                                            <div className="flex-auto w-80 pl-6 align-middle">
+                                                <Switch isSelected={dedup}
+                                                        className="text-white align-middle pt-1.5"
+                                                        onValueChange={setDedup}>
+                                                    <p className="text-white/80">Deduplicate</p>
+                                                </Switch>
+                                            </div>
                                         </div>
-                                        <div className="flex-auto w-80 pl-6 align-middle">
-                                            <Switch isSelected={dedup}
-                                                    className="text-white align-middle pt-1.5"
-                                                    onValueChange={setDedup}>
-                                                <p className="text-white/80">Deduplicate</p>
-                                            </Switch>
-                                        </div>
+                                        <TrafiServiceListGrouped key={host}
+                                                                 trafiServices={services}></TrafiServiceListGrouped>
                                     </div>
-                                    <TrafiServiceListGrouped key={host}
-                                                             trafiServices={services}></TrafiServiceListGrouped>
-                                </div>
-                                :
-                                <TrafiServiceListGrouped key={host} trafiServices={services}></TrafiServiceListGrouped>
-                            }
-                        </Tab>
-                    )}
+                                    :
+                                    <TrafiServiceListGrouped key={host} trafiServices={services}></TrafiServiceListGrouped>
+                                }
+                            </Tab>)
+                        })
+                    }
                 </Tabs>
             </div>
         </div>
