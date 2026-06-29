32d1f87963797a99ab6e47d2feff9dac0cc1065c
Bart Kaszewczuk <bartosz.kaszewczuk@icloud.com>
2026-06-29 22:28:16 +0200

    Extract traefik parser - Clean up (#47)

--- src/app/outgoing/traefik/TraefikParser.test.tsx
+++ src/app/outgoing/traefik/TraefikParser.test.tsx
@@ -8,10 +8,11 @@ describe('#getTrafiServices', () => {
         ["443", "Host(\`fake-domain.com\`)", ["https://fake-domain.com"]],
         ["8443", "Host(\`fake-domain.com\`)", ["https://fake-domain.com"]],
         ["443", "Host(\`abc.fake-domain.com\`)", ["https://abc.fake-domain.com"]],
-        ["443", "emptyRule", [""]],
+        ["443", "fakeEmptyRule", [""]],
         ["443", "Host(\`123.fake-domain.com\`) || Host(\`456.fake-domain.com\`)", ["https://123.fake-domain.com", "https://456.fake-domain.com"]]
     ])
-    ('should parse port %s and rule %s and return urls: %s', async (inputPort, inputRule, expected) => {
+    ('should parse port %s and rule %s and return urls: %s',
+        async (inputPort, inputRule, expected) => {
             expect(TraefikParser.getRoutesFromRule(inputRule, inputPort)).toStrictEqual(expected)
         }
     );
