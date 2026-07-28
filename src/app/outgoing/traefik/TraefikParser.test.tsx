0b7da9ec37023863bfa38dd3e32d180c4792f226
Bartosz Kaszewczuk <bartosz.kaszewczuk@icloud.com>
2026-07-28 14:36:23 +0200

    Add basic support for parsing traefik rules containing &&

--- src/app/outgoing/traefik/TraefikParser.test.tsx
+++ src/app/outgoing/traefik/TraefikParser.test.tsx
@@ -8,8 +8,10 @@ describe('#getTrafiServices', () => {
         ["443", "Host(\`fake-domain.com\`)", ["https://fake-domain.com"]],
         ["8443", "Host(\`fake-domain.com\`)", ["https://fake-domain.com"]],
         ["443", "Host(\`abc.fake-domain.com\`)", ["https://abc.fake-domain.com"]],
-        ["443", "fakeEmptyRule", [""]],
-        ["443", "Host(\`123.fake-domain.com\`) || Host(\`456.fake-domain.com\`)", ["https://123.fake-domain.com", "https://456.fake-domain.com"]]
+        ["443", "fakeEmptyRule", []],
+        ["443", "Host(\`123.fake-domain.com\`) || Host(\`456.fake-domain.com\`)", ["https://123.fake-domain.com", "https://456.fake-domain.com"]],
+        ["443", "Host(\`123.fake-domain.com\`) && Host(\`456.fake-domain.com\`)", ["https://123.fake-domain.com", "https://456.fake-domain.com"]],
+        ["443", "Host(\`123.fake-domain.com\`) && PathPrefix(`prefix`) || Host(\`456.fake-domain.com\`)", ["https://123.fake-domain.com", "https://456.fake-domain.com"]]
     ])
     ('should parse port %s and rule %s and return urls: %s',
         async (inputPort, inputRule, expected) => {
